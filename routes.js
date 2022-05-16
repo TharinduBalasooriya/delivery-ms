b3a4f9bc691d1fa64a2704f913af6eae5dfd2b24
TharinduBalasooriya <tharinduudana66@gmail.com>
2022-05-17 02:19:03 +0530

    add pipeLine

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ let router = require("express").Router();
 let diliveryController = require("./controller/dilivery.controller")
 
 router.get("/", (req, res) => {
-  res.send("Dilvery API v1");
+  res.send("Delivery API v1");
 });
 
 
-router.route("/dilivery").post(diliveryController.saveDilvery);
-router.route("/dilivery").get(diliveryController.getAllDiliveries);
-router.route("/dilivery/:id").get(diliveryController.getDilveryById);
-router.route("/dilivery/:id").delete(diliveryController.deleteDilveryById);
-router.route("/dilivery/:id").put(diliveryController.updateDilveryById);
+router.route("/delivery").post(diliveryController.saveDilvery);
+router.route("/delivery").get(diliveryController.getAllDiliveries);
+router.route("/delivery/:id").get(diliveryController.getDilveryById);
+router.route("/delivery/:id").delete(diliveryController.deleteDilveryById);
+router.route("/delivery/:id").put(diliveryController.updateDilveryById);
 module.exports = router;
